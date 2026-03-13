58085445c323c15a7b496782caaefcf0fd1b9c51
sunghun-kang <110204501+sunghun-kang@users.noreply.github.com>
2026-03-13 16:59:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,6 @@
-const CACHE = 'golf-journal-v1';
-const FILES = [
-  './golf-swing-journal_20a_20260310.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('activate', function(e) {
+  clients.claim();
 });
